7d2dfed60b2ea6f00d1b1e88db1c1312526ffe20
Adriana Saty Bertelli <adri-saty@hotmail.com>
2019-07-09 15:06:18 -0300

    added counter js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+let stickers = 0;
+let totalStickers = document.getElementById('stickers');
+
+function plus(){
+    stickers++;
+    totalStickers.value = stickers;
+}
+
+function minus(){
+    stickers <= 0 ? stickers = 0 : stickers--;
+    totalStickers.value = stickers;
+}
